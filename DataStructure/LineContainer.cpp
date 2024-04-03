aa021d20d34205fae4ab7f077329313d39c6a94a
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2024-04-02 18:51:52 -0700

    Add LineContainer

---
+++ DataStructure/LineContainer.cpp
@@ -0,0 +1,42 @@
+/*
+ * Line Container (Convex Hull Trick)
+ * Descripcion: Contenedor donde puedes anadir lineas en forma kx+m, y hacer
+ * consultas al valor maximo en un punto x.
+ * Pro-Tip: Si se busca el valor minimo en un punto x, anadir las lineas con
+ * pendiente K negativa (la consulta se dara de forma negativa)
+ * Tiempo: O(log n)
+ */
+
+struct Line {
+  mutable ll k, m, p;
+  bool operator<(const Line& o) const { return k < o.k; }
+  bool operator<(ll x) const { return p < x; }
+};
+
+struct LineContainer : multiset<Line, less<>> {
+  // (para doubles, usar inf = 1/.0, div(a,b) = a/b)
+  static const ll inf = LLONG_MAX;
+  ll div(ll a, ll b) {  // floored division
+    return a / b - ((a ^ b) < 0 && a % b);
+  }
+  bool isect(iterator x, iterator y) {
+    if (y == end()) return x->p = inf, 0;
+    if (x->k == y->k)
+      x->p = x->m > y->m ? inf : -inf;
+    else
+      x->p = div(y->m - x->m, x->k - y->k);
+    return x->p >= y->p;
+  }
+  void add(ll k, ll m) {
+    auto z = insert({k, m, 0}), y = z++, x = y;
+    while (isect(y, z)) z = erase(z);
+    if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+    while ((y = x) != begin() && (--x)->p >= y->p)
+      isect(x, erase(y));
+  }
+  ll query(ll x) {
+    assert(!empty());
+    auto l = *lower_bound(x);
+    return l.k * x + l.m;
+  }
+};
