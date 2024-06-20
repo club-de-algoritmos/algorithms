8d8661fa74f2ac9f4c91b0defe976d1e5add6fd7
Angel Olan <angel.edoc23@gmail.com>
2024-06-20 12:47:28 -0700

    Frac reorganizada. Se agrego mejor algoritmo de hashing

---
+++ DataStructure/Fraction.cpp
@@ -0,0 +1,22 @@
+/**
+ * Descripcion: estructura para manejar fracciones, es util cuando
+ * necesitamos gran precision y solo usamos fracciones
+ * Tiempo: O(1)
+ */
+struct Frac {
+  int num, den;
+
+  Frac(int num, int den) {
+    int g = gcd(num, den);
+    this->num = num / g;
+    this->den = den / g;
+  }
+
+  Frac operator*(Frac f) const { return Frac(num * f.num , den * f.den); }
+  Frac operator/(Frac f) const { return (*this) * Frac(f.den, f.num );  }
+  Frac operator+(Frac f) const { return Frac(num * f.den + den * f.num , den * f.den); }
+  Frac operator-(Frac f) const { return Frac(num * f.den - den * f.num , den * f.den); }
+  bool operator<(Frac& other) const { return num * other.den < other.num  * den; }
+  bool operator==(Frac& other) const { return num == other.num  && den == other.den; }
+  bool operator!=(Frac& other) const { return !(*this == other); }
+};
