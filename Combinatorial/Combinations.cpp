c5e3a92a3fcbf46ca9e9c992153d7329d8a21e89
Angel Olan <angel.edoc23@gmail.com>
2024-07-30 12:36:43 -0700

    Modified Combinations.cpp Deleted useless recursive approach

--- Combinatorial/Combinations.cpp
+++ Combinatorial/Combinations.cpp
@@ -11,13 +11,11 @@ ll invfact[MAX];
 void precalc_invfact() {
   precalc_inv();
   invfact[1] = 1;
-  for (int i = 2; i < MAX; i++)
-    invfact[i] = invfact[i - 1] * inv[i] % MOD;
+  for (int i = 2; i < MAX; i++) invfact[i] = invfact[i - 1] * inv[i] % MOD;
 }
 
 ll comb(int n, int k) {
-  if (n < k)
-    return 0;
+  if (n < k) return 0;
   return fact[n] * invfact[k] % MOD * invfact[n - k] % MOD;
 }
 
@@ -39,21 +37,10 @@ ll comb(int n, int k) {
  * no trabajamos con modulos (pues no tenemos una mejor opcion), usa DP.
  * Tiempo: O(n^2)
  */
-ll dp[MAX][MAX];
-ll comb(int n, int k) {
-  if (k > n || k < 0)
-    return 0;
-  if (n == k || k == 0)
-    return 1;
-  if (dp[n][k] != -1)
-    return dp[n][k];
-  return dp[n][k] = comb(n - 1, k) + comb(n - 1, k - 1);
-}
-
+ll c[MAX][MAX];
 void calc_comb() {
   FOR(i, 0, MAX) {
-    comb[i][0] = comb[i][i] = 1;
-    FOR(j, 1, i)
-    comb[i][j] = comb[i - 1][j] + comb[i - 1][j - 1];
+    c[i][0] = c[i][i] = 1;
+    FOR(j, 1, i) c[i][j] = c[i - 1][j] + c[i - 1][j - 1];
   }
 }
