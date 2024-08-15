ca1bec999034e60a62e30a42bc634cf128f4b412
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2024-08-14 18:43:38 -0700

    Added Segment Tree 2D (#30)
    * Modified Combinations.cpp
    Deleted useless recursive approach
    
    * Eliminated recursive nck function, iterative is faster
    
    * Added some graph theory
    
    * Updated Graphs/
    Deleted Korasaju.cpp, is slower than Tarjan and we may never use it.
    Renamed Tarjan.cpp as SCC.cpp
    Added Clique related algorithms
    Formatted files, now they cover less horizontal and vertical space
    Changed graph algorithms style, to avoid using global members
    Added MinimumVertexCover.cpp, to recover the MVC
    Refactor oldish-style of Bellman-Ford.cpp and Floyd-Warshall.cpp
    Switched to a most efficient centroid decomposition approach
    
    * Updated Segment Trees and deleted duplicated algorithm
    Normalized the format of all the Segment trees.
    Deleted the Sparse, Sparse Lazy and Persistent Lazy Segment Trees. The new ones are easily modifiable, if you need Sparse ST, use arrays L and R just like in the Persistent Version and add nodes incrementally, also, it is trivial to expand a ST to support lazy updates
    Deleted the directory DataStructure/SegmentTree, and moved all the files to DataStructure/
    Deleted duplicated algorithm DP/2DSum.cpp, it is equal to DataStructure/SubMatrix.cpp
    Added important comment on DataStructure/Fraction.cpp
    
    * Added DataStructure, modified and tested garph algorithms, documented Segment trees
    Added SparseSegmentTree.cpp
    Tested some algorithms
    Modified BinaryLiftingLCA.cpp and CentroidDecomposition.cpp, make the arrays global, passing through the functions provoked Stack Overflows
    Added some comments on the Segment tree implementations
    Added time complexity on MinimumVertexCover.cpp
    Modified Kruskal.cpp, used destructuration and updated the dsu calls according to the current DSU implementations
    Modified Dijkstra.cpp, the graph was passed as vector<pi> and not as vector<vector<pi>>
    
    * Updated BinaryLiftingLCA.cpp and EulerTour.cpp
    The graph was defined as vector<vi> g[maxn], instead of vi g[maxn]
    
    * Add Segment Tree 2D
    
    * Fixed SegmentTree2D.cpp description

---
+++ DataStructure/SegmentTree2D.cpp
@@ -0,0 +1,46 @@
+/**
+ * Descripcion: segment tree 2D para suma en un
+ * rectangulo y actualizacion en punto. 
+ * Implementacion iterativa para usar x4 menos memoria.
+ * Uso: STree st(n,m);st.build(a);st.query(x0,y0,x1,y1);
+ * (x0, y0) es la esquina superior izquierda (inclusiva)
+ * y (x1, y1) la esquina inferior derecha (exclusiva)
+ * Tiempo de construccion: O(nm)
+ * Tiempo de consulta y actualizacion: O(log n log m)
+ * Memoria total: O(nm)
+ * Status: testeado en CSES Forest Queries II
+ */
+#define NEUT 0
+struct STree {
+  int n, m;
+  vector<vi> st;
+  STree(int n, int m) : st(2*n, vi(2*m,NEUT)), n(n), m(m) {}
+  int comb(int x,int y){return x+y;}
+  void build(vector<vi>& a) {
+    FOR(i,0,n)FOR(j,0,m)
+      st[i+n][j+m]=a[i][j];
+    FOR(i,0,n)for(int j=m-1;j;--j)
+      st[i+n][j]=comb(st[i+n][j<<1],st[i+n][j<<1|1]);
+    for(int i=n-1;i;--i)FOR(j,0,2*m)
+      st[i][j]=comb(st[i<<1][j],st[i<<1|1][j]);
+  }
+  void upd(int x, int y, int v){
+    st[x+n][y+m]=v;
+    for(int j=y+m;j>1;j>>=1)st[x+n][j>>1]=comb(st[x+n][j],st[x+n][j^1]);
+    for(int i=x+n;i>1;i>>=1)for(int j=y+m;j;j>>=1)
+      st[i>>1][j]=comb(st[i][j],st[i^1][j]);
+  }
+  int query(int x0, int y0, int x1, int y1){
+    int r=0;
+    for(int i0=x0+n,i1=x1+n;i0<i1;i0>>=1,i1>>=1){
+      int t[4],q=0;
+      if(i0&1)t[q++]=i0++;
+      if(i1&1)t[q++]=--i1;
+      FOR(k,0,q)for(int j0=y0+m,j1=y1+m;j0<j1;j0>>=1,j1>>=1){
+        if(j0&1)r=comb(r,st[t[k]][j0++]);
+        if(j1&1)r=comb(r,st[t[k]][--j1]);
+      }
+    }
+    return r;
+  }
+};
