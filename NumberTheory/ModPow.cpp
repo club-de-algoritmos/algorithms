c896d263b11efe072772e508eb0731562fb1ca12
Angel Olan <angel.edoc23@gmail.com>
2024-08-28 00:12:58 -0700

    Refactor NumberTheory/ and added MD Theory file

--- NumberTheory/ModPow.cpp
+++ NumberTheory/ModPow.cpp
@@ -6,9 +6,7 @@ ll be(ll a, ll b, ll m) {
   ll res = 1;
   a %= m;
   while (b) {
-    if (b & 1) {
-      res = res * a % m;
-    }
+    if (b & 1) res = res * a % m;
     a = a * a % m;
     b >>= 1;
   }
