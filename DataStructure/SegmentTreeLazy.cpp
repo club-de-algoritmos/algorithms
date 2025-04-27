48dc5d0615e7fe00cfb800977fff8760019031a7
Angel Eduardo Olan Castro <99060518+AngelOlanC@users.noreply.github.com>
2025-04-26 21:00:34 -0700

    Refactor Segment Trees (#46)
    * Update Segment Trees and DP algorithms
    
    * Add Number theory and Graph algorithms
    
    * Add Implicit Treap
    
    * Fix indentation on Treap.cpp
    
    * Update MinCostMaxFlow.h
    
    * Update CRT.cpp
    
    * Refactor Segment Trees

--- DataStructure/SegmentTreeLazy.cpp
+++ DataStructure/SegmentTreeLazy.cpp
@@ -1,96 +1,55 @@
 /**
  * Descripcion: lazy segment tree para suma y
  * actualizacion en rango. Rangos no inclusivos a la der.
- * [a,b) es el rango de la query y [s, e) el del nodo
- * Si los incrementos pueden ser negativos o cero, 
- * utilizar arreglo extra has_lazy, o buscar otro neutral
- * Uso: STree st;st.init(arr);st.upd(0, n, 3);st.query(0, n);
- * Tiempo de construccion: O(n)
- * Tiempo de consulta y actualizacion: O(log n)
+ * Tiempo: O(log n)
  * Status: testeado en CSES Range Update Queries
  */
 struct STree {
-  #define ls (k << 1) + 1
-  #define rs (k << 1) + 2
-  #define lp ls, s, m
-  #define rp rs, m, e
+  #define ls 2 * v + 1
+  #define rs 2 * v + 2
+  #define lp ls, tl, tm
+  #define rp rs, tm, tr
+  #define NEUT 0
 
   int n;
   vector<int> st, lazy;
 
-  STree(int n) : n(n), st((n << 2) + 5), lazy((n << 2) + 5) {}
+  STree(int n) : n(n), st(4 * n), lazy(4 * n) {}
 
-  int merge(int x, int y) {
-    return x + y;
+  void apply(int v, int tl, int tr, int val) {
+    st[v] += val * (tr - tl);
+    lazy[v] += val;
   }
 
-  void pull(int k) {
-    st[k] = merge(st[ls], st[rs]);
-  }
-
-  void apply(int k, int s, int e, int v) {
-    st[k] += (e - s) * v;
-    lazy[k] += v;
-  }
-
-  void push(int k, int s, int e) {
-    if (lazy[k]) {
-      int m = (s + e) >> 1;
-      apply(lp, lazy[k]);
-      apply(rp, lazy[k]);
-      lazy[k] = 0;
+  void push(int v, int tl, int tr) {
+    if (lazy[v]) {
+      int tm = (tl + tr) / 2;
+      apply(lp, lazy[v]), apply(rp, lazy[v]);
+      lazy[v] = 0;
     }
   }
 
-  void build(int k, int s, int e, vector<int>& a) {
-    if (s + 1 == e) {
-      st[k] = a[s];
-      return;
-    }
-    int m = (s + e) >> 1;
-    build(lp, a);
-    build(rp, a);
-    pull(k);
+  int query(int v, int tl, int tr, int l, int r) {
+    if (tr <= l || r <= tl) return NEUT;
+    if (l <= tl && tr <= r) return st[v];
+    push(v, tl, tr);
+    int tm = (tl + tr) / 2;
+    return query(lp, l, r) + query(rp, l, r);
   }
-
-  int query(int k, int s, int e, int a, int b) {
-    if (a <= s && e <= b) {
-      return st[k];
-    }
-    push(k, s, e);
-    int m = (s + e) >> 1;
-    if (a >= m) {
-      return query(rp, a, b);
-    }
-    if (b <= m) {
-      return query(lp, a, b);
-    }
-    return merge(query(lp, a, b), query(rp, a, b));
+  
+  void upd(int v, int tl, int tr, int l, int r, int val) {
+    if (tr <= l || r <= tl) return;
+    if (l <= tl && tr <= r) { apply(v, tl, tr, val); return; }
+    push(v, tl, tr);
+    int tm = (tl + tr) / 2;
+    upd(lp, l, r, val), upd(rp, l, r, val);
+    st[v] = st[ls] + st[rs];
   }
-
-  void upd(int k, int s, int e, int a, int b, int v) {
-    if (a <= s && e <= b) {
-      apply(k, s, e, v);
-      return;
-    }
-    push(k, s, e);
-    int m = (s + e) >> 1;
-    if (a < m) {
-      upd(lp, a, b, v);
-    }
-    if (b > m) {
-      upd(rp, a, b, v);
-    }
-    pull(k);
+ 
+  int query(int l, int r) {
+    return query(0, 0, n, l, r);
   }
-
-  int query(int a, int b) {
-    return query(0, 0, n, a, b);
-  }
-  void upd(int a, int b, int v) {
-    upd(0, 0, n, a, b, v);
-  }
-  void build(vector<int>& a) {
-    build(0, 0, n, a);
+  void upd(int l, int r, int val) {
+    upd(0, 0, n, l, r, val);
   }
 };
--- DataStructure/SegmentTreeSparse.cpp
+++ DataStructure/SegmentTreeSparse.cpp
@@ -1,87 +1,55 @@
 /**
  * Descripcion: segment tree esparcido. Para suma en rango y
- * actualizacion en punto.
- * Un segment tree es esparcido cuando sus nodos se van creando
- * conforme se vayan utilizando, lo que permite manejar grandes
- * rangos de indices. Es util cuando en esta situacion necesitas
- * un algoritmo online (no se puede realizar compresion de
- * coordenadas). 
- * Para mejorar velocidad, usar arreglos.
- * [a,b) es el rango de la query y [s, e) el del nodo
- * Uso: STree st(1e18);st.upd(6, 3);st.query(5, 1e12);
- * Tiempo de consulta y actualizacion: O(log n)
+ * actualizacion en punto. Util para algoritmos online.
+ * Tiempo: O(log n)
  * Memoria total: O(qlog(n))
  * Status: testeado en CSES Salary Queries
  */
 struct STree {
-  #define ls st[k].l
-  #define rs st[k].r
-  #define lp ls, s, m
-  #define rp rs, m, e
+  #define ls st[v].l
+  #define rs st[v].r
+  #define lp ls, tl, tm
+  #define rp rs, tm, tr
   #define NEUT 0
- 
+
   struct Node {
-    int v, l, r;
-    Node() : v(NEUT), l(-1), r(-1) {}
+    int l, r, val;
+    Node() : l(-1), r(-1), val(NEUT) {}
   };
- 
+
   int n;
   vector<Node> st;
- 
+
   STree(int n) : n(n), st(1) {}
- 
-  int merge(int x, int y) {
-    return x + y;
+
+  int value(int v) {
+    return v == -1 ? NEUT : st[v].val;
   }
- 
-  int get(int k) {
-    return k == -1 ? NEUT : st[k].v;
+
+  int query(int v, int tl, int tr, int l, int r) {
+    if (tr <= l || r <= tl || v == -1) return NEUT;
+    if (l <= tl && tr <= r) return st[v].val;
+    int tm = (tl + tr) / 2;
+    return query(lp, l, r) + query(rp, l, r);
   }
- 
-  void pull(int k) {
-    st[k].v = merge(get(ls), get(rs));
-  }
- 
-  int addNode() {
-    st.emplace_back();
-    return (int)st.size() - 1;
-  }
- 
-  int query(int k, int s, int e, int a, int b) {
-    if (k == -1 || e <= a || s >= b) {
-      return NEUT;
+  
+  int upd(int v, int tl, int tr, int i, int val) {
+    if (v == -1) {
+      v = int(st.size());
+      st.emplace_back();
     }
-    if (a <= s && e <= b) {
-      return st[k].v;
-    }
-    int m = (s + e) >> 1;
-    return merge(query(lp, a, b), query(rp, a, b));
+    if (tl + 1 == tr) return st[v].val = val, v;
+    int tm = (tl + tr) / 2;
+    if (i < tm) ls = upd(lp, i, val);
+    else rs = upd(rp, i, val);
+    st[v].val = value(ls) + value(rs);
+    return v;
   }
  
-  void upd(int k, int s, int e, int i, int v) {
-    if (s + 1 == e) {
-      st[k].v += v;
-      return;
-    }
-    int m = (s + e) >> 1;
-    if (i < m) {
-      if (ls == -1) {
-        ls = addNode();
-      }
-      upd(lp, i, v);
-    } else {
-      if (rs == -1) {
-        rs = addNode();
-      }
-      upd(rp, i, v);
-    }
-    pull(k);
+  int query(int l, int r) {
+    return query(0, 0, n, l, r);
   }
- 
-  int query(int a, int b) {
-    return query(0, 0, n, a, b);
-  }
-  void upd(int i, int v) {
-    upd(0, 0, n, i, v);
+  void upd(int i, int val) {
+    upd(0, 0, n, i, val);
   }
 };
